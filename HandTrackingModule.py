8427754d8dcc154c8a311eacd328e26aff5f4a55
Punith1305 <vakacharlapunith13@gmail.com>
2024-06-25 19:14:10 +0530

    Add files via upload

---
+++ HandTrackingModule.py
@@ -0,0 +1,54 @@
+import mediapipe as mp
+import cv2
+
+class HandDetector:
+    def __init__(self, mode=False, maxhands=2, detectionCon=0.5, trackcon=0.5):
+        self.mode = mode
+        self.maxHands = maxhands
+        self.detectionCon = detectionCon
+        self.trackCon = trackcon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode, max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectionCon,
+                                        min_tracking_confidence=self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.dotsColor = self.mpDraw.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2)
+        self.lineColor = self.mpDraw.DrawingSpec(color=(.124, 252, 0), thickness=3, circle_radius=2)
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        self.detectedPoints = self.hands.process(imgRGB)
+        if self.detectedPoints.multi_hand_landmarks:
+            for handCoOrdinates in self.detectedPoints.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handCoOrdinates, self.mpHands.HAND_CONNECTIONS, self.dotsColor,
+                                               self.lineColor)
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+
+        lmList = []
+        if self.detectedPoints.multi_hand_landmarks:
+            myHand = self.detectedPoints.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+        return lmList
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+
+if __name__ == "__main__":
+    main()
---
+++ MailModule.py
@@ -0,0 +1,75 @@
+import smtplib
+import os
+import email
+import imaplib
+from AudioModule import ThunderAssistant as ta
+
+hostEmailId = "punithvakacharla@gmail.com"
+hostPassword = "tpnamketwzengpuq"
+toId = os.environ.get('tomail')
+
+
+class ThunderMail:
+    @staticmethod
+    def send_mail(fromAdd, toAdd, password, mailContext):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(fromAdd, password)
+        server.sendmail(from_addr=fromAdd, to_addrs=toAdd, msg=mailContext)
+        server.quit()
+
+    @staticmethod
+    def read_mail(emailId, password, count = 1):
+        imap = imaplib.IMAP4_SSL("imap.gmail.com")
+        result = imap.login(emailId, password)
+        imap.select('"[Gmail]/All Mail"', readonly=True)
+        response, messages = imap.search(None, 'Unseen')
+        messages = messages[0].split()
+        latest = int(messages[-1])
+        oldest = int(messages[0])
+        for i in range(latest, latest - count, -1):
+            res, msg = imap.fetch(str(i), "(RFC822)")
+            for response in msg:
+                if isinstance(response, tuple):
+                    try:
+                        msg = email.message_from_bytes(response[1])
+                        Date = str(msg["Date"])
+                        From = msg["From"]
+                        From = str(From)[:From.index("<")]
+                        Subject = str(msg["Subject"])
+                        print(f'Subject is :{Subject}')
+                        ta.speak("Subject is: ")
+                        ta.speak(Subject)
+                        print(f'mail from {From}')
+                        ta.speak("Mail from")
+                        ta.speak(From)
+                        # print(msg["Date"])
+                        # print(msg["From"])
+                        # print(msg["Subject"])
+                        print(f'mail on {Date}')
+                        print(f'mail from {From}')
+                    except:
+                        print("missing data in mail")
+
+                    # mail_from = str(msg["From"])
+                    # mail_from = mail_from[:mail_from.index("<")]
+                    # speak(f'mail from. {mail_from}')
+                    # print(f'Subject is. {str(msg["Subject"])}')
+                    # speak(f'Subject is. {str(msg["Subject"])}')
+
+            for part in msg.walk():
+                if part.get_content_type() == "text/plain":
+                    body = part.get_payload(decode=True)
+                    # print(body.decode("UTF-8"))
+                    Body = body.decode('utf-8')
+                    print(f'Body: \n{Body}')
+                    ta.speak("Body is :")
+                    ta.speak(Body)
+
+
+if __name__ == "__main__":
+    emailManager = ThunderMail()
+    emailManager.send_mail(hostEmailId, "punithvakacharla@gmail.com", hostPassword, "this is a testing mail")
+    emailManager.read_mail(hostEmailId, password=hostPassword)
