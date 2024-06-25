203ea14e8e518317c57de629e03a01b1dce2d181
Punith1305 <vakacharlapunith13@gmail.com>
2024-06-25 19:17:35 +0530

    Update AudioModule.py

--- AudioModule.py
+++ AudioModule.py
@@ -1,1 +1,26 @@
-print("hello world")
+import speech_recognition as sr
+import pyttsx3
+import threading
+
+
+class ThunderAssistant:
+    @staticmethod
+    def speak(message):
+        engine = pyttsx3.init()
+        engine.say(message)
+        engine.runAndWait()
+
+    @staticmethod
+    def listen():
+        r = sr.Recognizer()
+        mic = sr.Microphone(device_index=1)
+        with mic as source:
+            print("listening ...")
+            audio = r.listen(source, timeout=10)
+            text = r.recognize_google(audio)
+            return text
+
+
+if __name__ == "__main__":
+    print(ThunderAssistant.listen())
+    print("Done!")
